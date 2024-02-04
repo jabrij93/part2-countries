2de623c436f604ebd12930e38ee2aac2c67558f2
jabss <jabrijuhinin93@gmail.com>
2024-02-04 18:34:42 +0800

    display one info for 1 country

--- part2-countries/src/App.jsx
+++ part2-countries/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then(response => {
         console.log("SEE ALL DATA", response);
+        console.log("SEE ALL DATA NAME COMMON", response.data.map(item => item.name.common));
         setAllCountries(response.data);
       })
       .catch(error => {
@@ -30,16 +31,23 @@ const App = () => {
         country.name.common.toLowerCase().includes(value.toLowerCase())
       );
 
-    // Check the number of filtered countries
-    if (filtered.length > 10) {
-      // When there are more than 10 matches, display a specific message
-      setFilteredCountries(['too many matches, please be more specific']);
-    } else {
-      // When there are 10 or fewer matches, or no input, map to the "name" property correctly
-      const mappedFiltered = filtered.map(country => country.name.common); // Adjust based on actual data structure
-      setFilteredCountries(mappedFiltered);
+      if (filtered.length === 1) {
+        // Handle the case for exactly one match
+        const country = filtered[0];
+        setFilteredCountries([{
+          name: country.name.common,
+          capital: country.capital ? country.capital[0] : 'N/A',
+          area: country.area,
+          languages: country.languages ? Object.entries(country.languages).map(([code, name]) => name) : [],
+          flag: country.flags.png,
+        }]);
+      } else if (filtered.length > 10) {
+        setFilteredCountries(['too many matches, please be more specific']);
+      } else {
+        const mappedFiltered = filtered.map(country => country.name.common);
+        setFilteredCountries(mappedFiltered);
+      }
     }
-  }
   }, [value, allCountries]);
 
   const handleChange = (event) => {
@@ -50,9 +58,40 @@ const App = () => {
     <div>
       <input value={value} onChange={handleChange} />
       <pre>
-        {filteredCountries.length === 1 && filteredCountries[0] === 'too many matches, please be more specific'
-          ? filteredCountries[0] : JSON.stringify(filteredCountries, null, 2)}
+        {filteredCountries.length === 1 && typeof filteredCountries[0] === 'object'
+          ? (
+              <div>
+                <h2>{filteredCountries[0].name}</h2>
+                <p>Capital: {filteredCountries[0].capital}</p>
+                <p>Area: {filteredCountries[0].area} km²</p>
+                <p>Languages:</p>
+                <ul>
+                  {filteredCountries[0].languages.map((language, index) => (
+                    <li key={index}>{language}</li>
+                  ))}
+                </ul>
+                <img src={filteredCountries[0].flag} alt={`Flag of ${filteredCountries[0].name}`} style={{ width: '100px', height: 'auto' }} />
+              </div>
+            )
+          : filteredCountries[0] === 'too many matches, please be more specific'
+            ? filteredCountries[0]
+            : JSON.stringify(filteredCountries, null, 2)
+        }
       </pre>
+      {/* <pre>
+        {filteredCountries.length === 1 && typeof filteredCountries[0] === 'too many matches, please be more specific'
+          ? (
+              <div>
+                <h2>{filteredCountries[0].name}</h2>
+                <p>Capital: {filteredCountries[0].capital}</p>
+                <p>Area: {filteredCountries[0].area} km²</p>
+                <p>Languages: {filteredCountries[0].languages}</p>
+                <img src={filteredCountries[0].flag} alt={`Flag of ${filteredCountries[0].name}`} />
+              </div>
+            )
+          : JSON.stringify(filteredCountries, null, 2)
+        }
+      </pre> */}
     </div>
   );
 };
